da49360c6edc43d4cec18aa7d6d2dd8b1011471e
Supitchaya <supitchaya.ana@student.mahidol.edu>
2024-04-20 00:28:53 +0700

    page

--- lib/Loading.dart
+++ lib/Loading.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(key: GlobalKey(), title: 'Paco Money'),
+      home: MyHomePage(key: GlobalKey(), title: 'Paco Moneys'),
     );
   }
 }
