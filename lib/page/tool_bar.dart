1ec1479caaaba79ddc04d555aa61511a73590e7c
Supitchaya <supitchaya.ana@student.mahidol.edu>
2024-04-21 04:11:33 +0700

    edit tool bar

--- lib/page/tool_bar.dart
+++ lib/page/tool_bar.dart
@@ -1,83 +1,83 @@
-// import 'package:flutter/material.dart';
-// import 'package:paco_money/page/home_page.dart';
-// import 'package:paco_money/page/wallet.dart';
-// import 'package:paco_money/page/analytics.dart';
-// import 'package:paco_money/screen/login.dart';
+import 'package:flutter/material.dart';
+// import path หน้าที่ต้องการเชื่อม
+import 'package:paco_money/page/home_page.dart';
+import 'package:paco_money/page/wallet.dart';
+import 'package:paco_money/page/analytics.dart';
+import 'package:paco_money/screen/login.dart';
 
-// class Home extends StatefulWidget {
-//   Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
-//   @override
-//   State<Home> createState() => _HomeState();
-// }
+  @override
+  State<Home> createState() => _HomeState();
+}
 
-// class _HomeState extends State<Home> {
-//   final PageController _pageController = PageController();
-//   int _selectedIndex = 0;
+class _HomeState extends State<Home> {
+  final PageController _pageController = PageController();
+  int _selectedIndex = 0;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: PageView(
-//         controller: _pageController,
-//         children: [
-//           HomePage(),
-//           WalletPage(),
-//           AnalyticsPage(),
-//           LoginPage(),
-//         ],
-//       ),
-//       bottomNavigationBar: Align(
-//         alignment: Alignment.bottomCenter,
-//         child: Container(
-//           height: 66,
-//           width: MediaQuery.of(context).size.width,
-//           color: Color(0xFFFFF5C0),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               _buildToolbarButton('image/book.png', 'Home', 0),
-//               _buildToolbarButton('image/wallet.png', 'Wallet', 1),
-//               _buildToolbarButton('image/analytics.png', 'Analytics', 2),
-//               _buildToolbarButton('image/logout.png', 'Logout', 3),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: [
+          Home(), // เปลี่ยน Home() เป็น HomePage()
+          WalletPage(),
+          Analytics(), // เปลี่ยน Analytics() เป็น AnalyticsPage()
+          LoginScreen(), // เปลี่ยน LoginScreen() เป็น Login()
+        ],
+      ),
+      bottomNavigationBar: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 66,
+          width: MediaQuery.of(context).size.width,
+          color: Color(0xFFFFF5C0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildToolbarButton('image/book.png', 'Home', 0),
+              _buildToolbarButton('image/wallet.png', 'Wallet', 1),
+              _buildToolbarButton('image/analytics.png', 'Analytics', 2),
+              _buildToolbarButton('image/logout.png', 'Logout', 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-//   Widget _buildToolbarButton(String iconPath, String text, int index) {
-//     return GestureDetector(
-//       onTap: () {
-//         _pageController.animateToPage(
-//           index,
-//           duration: Duration(milliseconds: 500),
-//           curve: Curves.ease,
-//         );
-//       },
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 24),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Image.asset(
-//               iconPath,
-//               width: 35,
-//               height: 35,
-//             ),
-//             SizedBox(height: 5),
-//             Text(
-//               text,
-//               style: TextStyle(
-//                 fontSize: 12,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+  Widget _buildToolbarButton(String iconPath, String text, int index) {
+    return GestureDetector(
+      onTap: () {
+        _pageController.animateToPage(
+          index,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              iconPath,
+              width: 35,
+              height: 35,
+            ),
+            SizedBox(height: 5),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
